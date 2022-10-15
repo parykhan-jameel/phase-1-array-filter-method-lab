cbdfd3393211d8ffbfeafe7a65d1b23aa4f16c62
parykhan-jameel <parexan.00443431@gmail.com>
2022-10-15 14:23:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+const findMatching = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        return filtered.toUpperCase() === str.toUpperCase();
+        // if(filtered.toUpperCase() === str.toUpperCase()) {
+        //     console.log('coooool');
+        // }
+    });
+    return filterArr;
+}
+
+const fuzzyMatch = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        return filtered[0] === str[0];
+    });
+    return filterArr;
+}
+
+const matchName = (drivers, str) => {
+    const filterArr = drivers.filter((filtered) => {
+        console.log(filtered.name);
+        return filtered.name === str;
+    });
+    return filterArr;
+}
